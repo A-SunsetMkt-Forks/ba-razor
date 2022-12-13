86ac538127579d9807bf6498a4ab16c36403ac3c
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-12-13 22:22:24 +0900

    feat: 自动切换源分支来补充 momotalk 缺失字段

--- razor.py
+++ razor.py
@@ -7,6 +7,8 @@ from colorama import Fore
 from momotalk import process_momotalk
 from utils import MyDumper
 
+import git as Git
+
 
 @click.group()
 def _root():
@@ -23,12 +25,48 @@ def _momotalk(source: str, destination: str):
     output_resource_path = pathlib.Path(destination)
     output_resource_path.mkdir(parents=True, exist_ok=True)
 
-    momotalk_outputs = process_momotalk(source)
-    for momotalk_output in momotalk_outputs:
-        file_path = output_resource_path / f"{momotalk_output.CharacterId}.yml"
-        print(f"{Fore.BLUE}Parse data to yaml and write to file [{file_path}]{Fore.RESET}")
-        with open(file_path, mode="w") as fd:
-            yaml.dump(momotalk_output.dict(), fd, sort_keys=False, allow_unicode=True, Dumper=MyDumper)
+    def write_momotalk(src=source):
+        momotalk_outputs = process_momotalk(src)
+        for momotalk_output in momotalk_outputs:
+            file_path = output_resource_path / f"{momotalk_output.CharacterId}.yml"
+            print(f"{Fore.BLUE}Parse data to yaml and write to file [{file_path}]{Fore.RESET}")
+            with open(file_path, mode="w") as fd:
+                yaml.dump(momotalk_output.dict(), fd, sort_keys=False, allow_unicode=True, Dumper=MyDumper)
+
+    # 检查源目录是否是 git 仓库
+    try:
+        repo = Git.repo.Repo(source)
+        git = repo.git
+        # 如果是 git 仓库，检查当前分支名
+        current_branch = git.rev_parse("--abbrev-ref", "HEAD")
+        # 获取所有的分支名
+        branch_names = [branch.name for branch in repo.branches]
+
+        # 如果分支名是 jp，直接进行一次写入
+        if "jp" == current_branch:
+            write_momotalk(source)
+            # 如果 global 分支存在，切换到 global 分支，进行一次写入。这样国际服的翻译就能覆盖日服文件的空白字段
+            if "global" in branch_names:
+                click.echo(f"{Fore.YELLOW}Switch to global branch{Fore.RESET}")
+                git.checkout("global")
+                write_momotalk(source)
+                # 切换回 jp 分支
+                click.echo(f"{Fore.YELLOW}Switch back to jp branch{Fore.RESET}")
+                git.checkout("jp")
+        # 如果分支名是 global 并且 jp 分支存在，切换到 jp 分支，进行一次写入
+        elif "global" == current_branch and "jp" in branch_names:
+            click.echo(f"{Fore.YELLOW}Switch to jp branch{Fore.RESET}")
+            git.checkout("jp")
+            write_momotalk(source)
+            # 切换回 global 分支
+            click.echo(f"{Fore.YELLOW}Switch back to global branch{Fore.RESET}")
+            git.checkout("global")
+            # 写入
+            write_momotalk(source)
+    # 如果不是 git 仓库，直接写入
+    except (Git.exc.InvalidGitRepositoryError, Git.exc.NoSuchPathError):
+        click.echo(f"{Fore.RED}Source is not a git repository: {source}{Fore.RESET}")
+        write_momotalk(source)
 
 
 @_root.command("scenario")
