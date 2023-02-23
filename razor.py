254039aa16591e6eb5e41b67004696b4e7b4c687
notnotype <notnotype@gmail.com>
2023-02-23 12:11:42 +0800

    fix: main_scenario

--- razor.py
+++ razor.py
@@ -8,6 +8,7 @@ import model
 from functions.amend import amend_momotalk
 from functions.momotalk import process_momotalk
 from functions.favor_scenario import process_favor_scenario
+from functions.main_scenario import process_main_scenario
 from functions.rename import rename_momotalk
 from format_parser import save_yaml, save_json, JsonParser, YamlParser
 
@@ -44,14 +45,14 @@ def _favor_scenario(source: str, destination: str):
 
     :arg dumper: yaml dumper: CDumper(faster) | Dumper | MyDumper(slowest)
     """
-    click.echo("Razing momotalk")
+    click.echo("Razing favor scenario")
     output_resource_path = pathlib.Path(destination)
     output_resource_path.mkdir(parents=True, exist_ok=True)
 
     favor_scenario_outputs = process_favor_scenario(source)
     for favor_scenario_output in favor_scenario_outputs:
         file_path = output_resource_path / f"{favor_scenario_output.GroupId}.json"
-        print(f"{Fore.BLUE}Parse data to yaml and write to file [{file_path}]{Fore.RESET}")
+        print(f"{Fore.BLUE}Parse data to json and write to file [{file_path}]{Fore.RESET}")
         save_json(favor_scenario_output, file_path)
 
 @_root.command("main_scenario")
@@ -62,14 +63,14 @@ def _main_scenario(source: str, destination: str):
 
     :arg dumper: yaml dumper: CDumper(faster) | Dumper | MyDumper(slowest)
     """
-    click.echo("Razing momotalk")
+    click.echo("Razing main scenario")
     output_resource_path = pathlib.Path(destination)
     output_resource_path.mkdir(parents=True, exist_ok=True)
 
-    favor_scenario_outputs = process_favor_scenario(source)
+    favor_scenario_outputs = process_main_scenario(source)
     for favor_scenario_output in favor_scenario_outputs:
         file_path = output_resource_path / f"{favor_scenario_output.GroupId}.json"
-        print(f"{Fore.BLUE}Parse data to yaml and write to file [{file_path}]{Fore.RESET}")
+        print(f"{Fore.BLUE}Parse data to json and write to file [{file_path}]{Fore.RESET}")
         save_json(favor_scenario_output, file_path)
 
 @_root.command("amend_momotalk")
