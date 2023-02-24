c25faa207a54fd71aaae9b3170666d61a4e9d6a6
notnotype <notnotype@gmail.com>
2023-02-24 10:58:41 +0800

    update ci

--- process_all.sh
+++ process_all.sh
@@ -40,24 +40,24 @@ git fetch
 git switch global
 git reset --hard HEAD
 cd "${script_dir}"
-python razor.py momotalk -s "${git_path}" -o "./output/momotalk_gl"
+poetry run python razor.py momotalk -s "${git_path}" -o "./output/momotalk_gl"
 # favor scenario
-python razor.py favor_scenario -s "${git_path}" -o "./output/favor_scenario_gl"
+poetry run python razor.py favor_scenario -s "${git_path}" -o "./output/favor_scenario_gl"
 # main scenario
-python razor.py main_scenario -s "${git_path}" -o "./output/main_scenario_gl"
+poetry run python razor.py main_scenario -s "${git_path}" -o "./output/main_scenario_gl"
 
 # jp
 cd "${git_path}"
 git switch jp
 git reset --hard HEAD
 cd "${script_dir}"
-python razor.py momotalk -s "${git_path}" -o "./output/momotalk_jp"
+poetry run python razor.py momotalk -s "${git_path}" -o "./output/momotalk_jp"
 # favor scenario
-python razor.py favor_scenario -s "${git_path}" -o "./output/favor_scenario_jp"
+poetry run python razor.py favor_scenario -s "${git_path}" -o "./output/favor_scenario_jp"
 # main scenario
-python razor.py main_scenario -s "${git_path}" -o "./output/main_scenario_jp"
+poetry run python razor.py main_scenario -s "${git_path}" -o "./output/main_scenario_jp"
 
 # amend
-python razor.py amend_momotalk -s "./output/momotalk_jp" -a "./output/momotalk_gl"
-python razor.py amend_scenario -s "./output/main_scenario_jp" -a "./output/main_scenario_gl"
-python razor.py amend_scenario -s "./output/favor_scenario_jp" -a "./output/favor_scenario_gl"
+poetry run python razor.py amend_momotalk -s "./output/momotalk_jp" -a "./output/momotalk_gl"
+poetry run python razor.py amend_scenario -s "./output/main_scenario_jp" -a "./output/main_scenario_gl"
+poetry run python razor.py amend_scenario -s "./output/favor_scenario_jp" -a "./output/favor_scenario_gl"
